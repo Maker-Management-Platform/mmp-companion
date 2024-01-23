d2fbce86e3553dc1f7c17882fdfd49cbaead4eb0
Eduardo Oliveira <eduardoliveira2009@gmail.com>
2024-01-23 15:30:15 +0000

    #4

--- main.js
+++ main.js
@@ -10,6 +10,12 @@ const init = async () => {
     console.log(settings)
     const tabs = await chrome.tabs.query({ active: true })
     for (const i in tabs) {
+        if (tabs[i].title === "MMP - Maker Management Platform") {
+            await showInit()
+            return
+        }
+    }
+    for (const i in tabs) {
         const url = new URL(tabs[i].url);
         if (supportedSites.includes(url.origin)) {
             await actions.show[url.origin]()
@@ -44,6 +50,9 @@ const showInit = async () => {
                 const url = new URL(tabs[i].url);
                 const response = await fetch(url.origin + '/settings.json')
                 settings = await response.json();
+                if (settings.local_backend.startsWith("/")) {
+                    settings.local_backend = url.origin + settings.local_backend
+                }
                 await chrome.storage.sync.set({ settings })
                 init()
             }
@@ -124,9 +133,9 @@ actions.show['https://www.thingiverse.com'] = async () => {
 
 const mmpBackendStorage = await chrome.storage.sync.get("settings")
 console.log(mmpBackendStorage)
-if (!mmpBackendStorage.settings) {
-    await showInit()
-} else {
+
+if (mmpBackendStorage.settings) {
     settings = mmpBackendStorage.settings
-    await init()
 }
+
+await init()
